96cf30c7a1f64721e2443becd23abb29fca6af2a
Maria Fonseca <maria.fonseca@cognite.com>
2023-08-15 14:51:34 +0200

    transformation

--- datamodels/IceCream/transformations/Populate_data/transformation.sql
+++ datamodels/IceCream/transformations/Populate_data/transformation.sql
@@ -1,1 +1,7 @@
-select 1 as key, $SPACE as name, 'asd' as externalId
+select
+  cast(`external_id` as STRING) as externalId,
+  cast(`description` as STRING) as description,
+  cast(`name` as STRING) as name,
+  cast(`metadata` as STRING) as metadata
+from
+  `src:001:sap:db`.`site`;
